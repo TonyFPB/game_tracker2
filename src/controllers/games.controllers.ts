956ab3a3d4f7fecd891295f108d561e2cbabdfa2
Antonio Breyer <antoniofabiopbreyer@gmail.com>
2023-01-19 00:49:44 -0300

    feat: get all games and name by query

--- src/controllers/games.controllers.ts
+++ src/controllers/games.controllers.ts
@@ -1,7 +1,27 @@
 import { Request, Response } from "express";
 import { GameInsertType } from "../protocols/game.protocols.js";
-import { insertGame } from "../repositories/games.repositories.js";
+import { findAllGames, findGamesWithQuery, insertGame } from "../repositories/games.repositories.js";
 
+
+export async function getGame(req: Request, res: Response) {
+    const { game } = req.query
+    try {
+        if (game) {
+            const games = await findGamesWithQuery(game.toString().toLowerCase())
+            if (games.rowCount === 0){
+                return res.sendStatus(404)
+            }
+            return res.send(games.rows)
+        }
+        if (!game) {
+            const games = await findAllGames()
+            return res.send(games.rows)
+        }
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+}
 
 export async function postGame(req: Request, res: Response) {
     const game = res.locals as GameInsertType
--- src/repositories/games.repositories.ts
+++ src/repositories/games.repositories.ts
@@ -1,6 +1,6 @@
 import { QueryResult } from "pg";
 import connection from "../database/data.js";
-import { GameEntity } from "../protocols/game.protocols.js";
+import { GameEntity, GamesAll } from "../protocols/game.protocols.js";
 
 export async function findGameByName(gameName: string): Promise<QueryResult<GameEntity>> {
     return connection.query("SELECT * FROM games WHERE name=$1", [gameName])
@@ -10,3 +10,10 @@ export async function insertGame(name: string, type_id: number) {
     return connection.query('INSERT INTO games (name,type_id) VALUES ($1,$2)', [name, type_id])
 }
 
+export async function findAllGames(): Promise<QueryResult<GamesAll>> {
+    return connection.query("SELECT g.id,INITCAP(g.name) AS name,g.completed,g.type_id, t.name AS type FROM games g JOIN types t ON g.type_id=t.id ORDER BY g.id;")
+}
+
+export async function findGamesWithQuery(game:string): Promise<QueryResult<GamesAll>> {
+    return connection.query("SELECT g.id,INITCAP(g.name) AS name,g.completed,g.type_id, t.name AS type FROM games g JOIN types t ON g.type_id=t.id WHERE g.name LIKE $1 ORDER BY g.id;",[`${game}%`])
+}
